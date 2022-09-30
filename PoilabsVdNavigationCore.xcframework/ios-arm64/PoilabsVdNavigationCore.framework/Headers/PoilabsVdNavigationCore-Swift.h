232c83efc7e2340bb4578dc6877c3a00f4d94556
runner <runner@Mac-1664544920910.local>
2022-09-30 13:37:19 +0000

    4.1.1

--- PoilabsVdNavigationCore.xcframework/ios-arm64/PoilabsVdNavigationCore.framework/Headers/PoilabsVdNavigationCore-Swift.h
+++ PoilabsVdNavigationCore.xcframework/ios-arm64/PoilabsVdNavigationCore.framework/Headers/PoilabsVdNavigationCore-Swift.h
@@ -448,6 +448,7 @@ SWIFT_CLASS("_TtC23PoilabsVdNavigationCore7PlaceMO")
 @property (nonatomic, copy) NSString * _Nullable id;
 @property (nonatomic, copy) NSString * _Nullable categoryTitle;
 @property (nonatomic, copy) NSString * _Nullable categoryKey;
+@property (nonatomic, copy) NSString * _Nullable categoryTitleEn;
 @property (nonatomic, copy) NSArray<NSNumber *> * _Nullable major;
 @property (nonatomic, copy) NSString * _Nullable address;
 @property (nonatomic) double lat;
