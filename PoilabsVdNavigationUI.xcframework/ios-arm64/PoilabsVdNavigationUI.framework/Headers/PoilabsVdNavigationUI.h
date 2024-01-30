a692dce26ea9ea83245d0c5b1807191134b37d2c
runner <runner@Mac-1706605771801.local>
2024-01-30 09:24:47 +0000

    4.4.1

--- PoilabsVdNavigationUI.xcframework/ios-arm64/PoilabsVdNavigationUI.framework/Headers/PoilabsVdNavigationUI.h
+++ PoilabsVdNavigationUI.xcframework/ios-arm64/PoilabsVdNavigationUI.framework/Headers/PoilabsVdNavigationUI.h
@@ -15,4 +15,4 @@ FOUNDATION_EXPORT const unsigned char PoilabsVdNavigationUIVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PoilabsVdNavigationUI/PublicHeader.h>
 
-
+#import "StepSlider.h"
